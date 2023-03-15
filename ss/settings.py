b3ebb023902b9ab774c3cf9af6c0a56d0d35e70b
Aalaap Hegde <aalaaphegde@Aalaaps-MacBook-Pro.local>
2023-03-15 12:56:08 -0700

    staticfiles

--- ss/settings.py
+++ ss/settings.py
@@ -116,4 +116,17 @@ LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+MEDIA_URL = '/media/' 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
